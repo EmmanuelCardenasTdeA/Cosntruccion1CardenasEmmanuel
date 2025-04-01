94a246b0ea4b77071072d1b97b1181947c120523
EmmanuelCardenasTdeA <emmanuel.cardenas@correo.tdea.edu.co>
2025-03-31 21:48:25 -0500

    ADD BD

--- VeterinatyApp/src/main/resources/veterinarybd.sql
+++ VeterinatyApp/src/main/resources/veterinarybd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-03-2025 a las 22:38:52
+-- Tiempo de generación: 01-04-2025 a las 04:44:57
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -47,7 +47,9 @@ CREATE TABLE `clinica` (
 INSERT INTO `clinica` (`clinica_id`, `date`, `consultation`, `syntomatology`, `diagnostic`, `treatment`, `orden_id`, `vacum_history`, `allergy_medicines`, `details_treatement`) VALUES
 (1, '2025-03-20 02:43:15', 'consulta', 'sintomatologia', 'diagnostico', 'tratamiento', 1, 'vacuna 1, vacuna 2', 'alergia 1, alergia 2', 'detalles del tratamiento'),
 (2, '2025-03-20 03:32:16', 'consulta por vacunacion', 'vacunacion', 'vacunacion', 'vacunacion', 2, 'Vacuna 1, Vacunacion 2', 'Alergia', 'Vacunacion'),
-(3, '2025-03-20 03:35:27', 'EDITADA', 'EDITADA', 'EDITADA', 'EDITADA', 3, 'No aplica', 'EDITADA', 'EDITADA');
+(3, '2025-03-20 03:35:27', 'EDITADA', 'EDITADA', 'EDITADA', 'EDITADA', 3, 'No aplica', 'EDITADA', 'EDITADA'),
+(5, '2025-04-01 02:41:38', 'Revision', 'N/A', 'Sano', 'N/a', 5, 'N/A', 'N/A', 'N/A'),
+(6, '2025-04-01 02:43:55', 'Rutina', 'N/A', 'Sano', 'N/A', 6, 'N/A', 'N/A', 'N/A');
 
 -- --------------------------------------------------------
 
@@ -95,7 +97,10 @@ CREATE TABLE `orden` (
 INSERT INTO `orden` (`orden_id`, `pet_id`, `document`, `user_id`, `medication_name`, `medication_dosis`, `orden_status`, `date`) VALUES
 (1, 1, 1, 4, 'dosis', 1, 'Anulada', '2025-03-13 21:28:19'),
 (2, 2, 2, 4, 'Medicamento', 1.4, 'Activa', '2025-03-20 03:28:03'),
-(3, 3, 2, 4, 'NOVACUNA', 12, 'Activa', '2025-03-20 03:34:52');
+(3, 3, 2, 4, 'NOVACUNA', 12, 'Activa', '2025-03-20 03:34:52'),
+(4, 2, 123, 8, '123', 123, 'Activa', '2025-04-01 01:26:07'),
+(5, 6, 100411, 6, 'N/A', 0, 'Activa', '2025-04-01 02:40:52'),
+(6, 7, 100411, 4, 'N/A', 0, 'Activa', '2025-04-01 02:43:12');
 
 -- --------------------------------------------------------
 
@@ -119,7 +124,9 @@ INSERT INTO `person` (`document`, `name`, `age`) VALUES
 (3, 'vetprueba', 12),
 (4, 'vendedorprueba', 1),
 (5, 'vet', 5),
-(6, 'seller', 6);
+(6, 'seller', 6),
+(123, '123', 123),
+(100411, 'Emmanuel Cardenas', 23);
 
 -- --------------------------------------------------------
 
@@ -145,7 +152,10 @@ INSERT INTO `pet` (`document`, `pet_id`, `pet_name`, `pet_age`, `species`, `pet_
 (1, 1, 'Prueba', 1, 'ES', 'RA', 1.2),
 (2, 2, 'MascotaDePrueba2', 2, 'Perro', 'Chihuaha', 1.5),
 (2, 3, 'MascotaDePrueba3', 3, 'Ave', 'pelicano', 15.23),
-(1, 4, 'Prueba3', 3, 'Gato', 'siames', 15);
+(1, 4, 'Prueba3', 3, 'Gato', 'siames', 15),
+(123, 5, '123', 123, 'Perro', '1233', 123),
+(100411, 6, 'Dino', 1, 'Perro', 'San Bernardo', 60),
+(100411, 7, 'Doki', 1, 'Perro', 'San bernardo', 50);
 
 -- --------------------------------------------------------
 
@@ -170,7 +180,8 @@ INSERT INTO `user` (`user_id`, `document`, `user_name`, `password`, `role`) VALU
 (4, 3, 'vetuser', 'vetpass', 'veterinary'),
 (5, 4, 'vendedoruser', 'vendedorpass', 'seller'),
 (6, 5, 'vet', 'vet', 'veterinary'),
-(7, 6, 'seller', 'seller', 'seller');
+(7, 6, 'seller', 'seller', 'seller'),
+(8, 123, '123', '123', 'veterinary');
 
 --
 -- Índices para tablas volcadas
@@ -220,7 +231,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT de la tabla `clinica`
 --
 ALTER TABLE `clinica`
-  MODIFY `clinica_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `clinica_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `invoiced`
@@ -232,19 +243,19 @@ ALTER TABLE `invoiced`
 -- AUTO_INCREMENT de la tabla `orden`
 --
 ALTER TABLE `orden`
-  MODIFY `orden_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `orden_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `pet`
 --
 ALTER TABLE `pet`
-  MODIFY `pet_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `pet_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `user_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
